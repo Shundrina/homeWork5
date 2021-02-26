109bcc9b48a5ed5e41ca4c2f3e3b9bcefdec6d7a
Dariia <lystopadina@gmail.com>
2021-02-26 23:27:31 +0200

    homework12

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     age = models.IntegerField()
